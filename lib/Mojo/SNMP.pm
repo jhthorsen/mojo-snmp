00f6f12d11c9680439837f12090eaf018c2baba5
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-09-26 15:36:37 +0200

    Released version 0.04
           * Add support for passing maxrepetitions to prepare()
           * Add add_custom_request_method() method
           * Add custom method bulk_walk() - Espen Tallaksen

--- lib/Mojo/SNMP.pm
+++ lib/Mojo/SNMP.pm
@@ -6,7 +6,7 @@ Mojo::SNMP - Run SNMP requests with Mojo::IOLoop
 
 =head1 VERSION
 
-0.0301
+0.04
 
 =head1 SYNOPSIS
 
@@ -90,7 +90,7 @@ use Scalar::Util ();
 use constant DEBUG => $ENV{MOJO_SNMP_DEBUG} ? 1 : 0;
 use constant MAXREPETITIONS => 10;
 
-our $VERSION = '0.0301';
+our $VERSION = '0.04';
 
 my @EXCLUDE_METHOD_ARGS = qw( maxrepetitions );
 my %EXCLUDE = (
